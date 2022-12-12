fd19d7ae378ecdbb2c52c110fa5b959bab6ad13b
Robert Stiff <uatecuk@gmail.com>
2022-12-12 11:23:19 +0000

    obviously we must host the controller

--- HomepageSidecar/Program.cs
+++ HomepageSidecar/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Hosting;
 Host.CreateDefaultBuilder(args)
     .ConfigureServices((hostContext, services) =>
     {
+        services.AddHostedService<Controller>();
         services.AddSingleton<IKubernetes>(new Kubernetes(KubernetesClientConfiguration.InClusterConfig()));
     })
     .Build()
