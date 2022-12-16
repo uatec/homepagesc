f67e765569abcaea0dcce767ff3da5738e8eb30e
Robert Stiff <uatecuk@gmail.com>
2022-12-16 21:53:02 +0000

    depenndencies

--- HomepageSidecar/Program.cs
+++ HomepageSidecar/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Options;
 Host.CreateDefaultBuilder(args)
     .ConfigureServices((hostContext, services) =>
     {
+        services.AddSingleton<ConfigBuilder>();
         services.AddHostedService<Controller>();
         services.AddSingleton<IKubernetes>(s =>
         {
